281a092eb99c027468e35468ebc3e34fed92f004
shikhar0507 <kap.shikhar@gmail.com>
2018-08-31 12:48:39 +0530

    sw version is 16

--- public/syncWorker.js
+++ public/syncWorker.js
@@ -1,6 +1,6 @@
 (function () {
 
-    const CACHE_NAME = 'V15'
+    const CACHE_NAME = 'V16'
     const urlsToCache = [
       'external/firebase-app.js',
       'external/firebasejs/5.2.0/firebase-auth.js',
