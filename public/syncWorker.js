a673abebb53e3f8c2db4fc88b158d0de8612bf15
shikhar0507 <kap.shikhar@gmail.com>
2018-09-14 11:44:17 +0530

    removed unused npm packages

--- public/syncWorker.js
+++ public/syncWorker.js
@@ -1,6 +1,6 @@
 (function () {
 
-    const CACHE_NAME = 'V40';
+    const CACHE_NAME = 'V41';
     const urlsToCache = [
         'https://www.gstatic.com/firebasejs/5.4.1/firebase-app.js',
         'https://www.gstatic.com/firebasejs/5.4.1/firebase-auth.js',
