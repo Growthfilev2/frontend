8adac38a8c8d55e024529a4a0d7100d3d687e55f
shikhar0507 <kap.shikhar@gmail.com>
2018-12-17 12:25:24 +0530

    commented out webkit handler for development

--- src/js/services.js
+++ src/js/services.js
@@ -472,7 +472,7 @@ function isLocationVerified(reqType) {
     }
     return true;
   }
-  webkit.messageHandlers.checkInternet.postMessage(reqType);
+  // webkit.messageHandlers.checkInternet.postMessage(reqType);
   return true;
 }
 
