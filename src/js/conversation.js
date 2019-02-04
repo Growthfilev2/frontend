c45bd71b7fa37b17c0029191f92173bc3cc56ffa
shikhar0507 <kap.shikhar@gmail.com>
2019-02-04 13:02:25 +0530

    removed instant calls from conversation.js

--- src/js/conversation.js
+++ src/js/conversation.js
@@ -9,11 +9,7 @@ function conversation(id, pushState) {
     } else {
       listView()
     }
-  }).catch(function (error) {
-    requestCreator('instant', JSON.stringify({
-      message: error
-    }))
-  })
+  }).catch(handleError)
 }
 
 function checkIfRecordExists(store, id) {
@@ -506,11 +502,7 @@ function createHeaderContent(db, id) {
           } else {
             listView()
           }
-        }).catch(function (error) {
-          requestCreator('instant', JSON.stringify({
-            message: error
-          }))
-        })
+        }).catch(handleError)
       })
     })
   }
@@ -752,12 +744,7 @@ function fillUsersInSelector(data, dialog) {
             key: data.attachment.key
           }, {
             primary: JSON.parse(radio.value)
-          }).then(removeDialog).catch(function (error) {
-            requestCreator('instant', JSON.stringify({
-              message: error
-            }))
-
-          })
+          }).then(removeDialog).catch(handleError)
           return;
         }
         if (data.record.hasOwnProperty('create')) {
@@ -766,11 +753,7 @@ function fillUsersInSelector(data, dialog) {
               hash: 'addOnlyAssignees',
             }, {
               primary: selectedPeople
-            }).then(removeDialog).catch(function (error) {
-              requestCreator('instant', JSON.stringify({
-                message: error
-              }))
-            })
+            }).then(removeDialog).catch(handleError)
 
           })
           return
@@ -856,11 +839,7 @@ function addNewNumber(data) {
             key: data.attachment.key
           }, {
             primary: [formattedNumber]
-          }).then(removeDialog).catch(function (error) {
-            requestCreator('instant', JSON.stringify({
-              message: error
-            }))
-          })
+          }).then(removeDialog).catch(handleError)
           return
         }
 
@@ -869,11 +848,7 @@ function addNewNumber(data) {
             hash: 'addOnlyAssignees',
           }, {
             primary: [formattedNumber]
-          }).then(removeDialog).catch(function (error) {
-            requestCreator('instant', JSON.stringify({
-              message: error
-            }))
-          })
+          }).then(removeDialog).catch(handleError)
           return
         }
         if (isLocationVerified()) {
@@ -1076,11 +1051,7 @@ function handleClickListnersForMap(db, dialog, data) {
         address: selectedField.address,
         geopoint: selectedField.geopoint
       },
-    }).then(removeDialog).catch(function (error) {
-      requestCreator('instant', JSON.stringify({
-        message: error
-      }))
-    })
+    }).then(removeDialog).catch(handleError)
   }
 }
 
@@ -1110,11 +1081,7 @@ function fillChildrenInSelector(selectorStore, activityRecord, dialog, data) {
       key: data.attachment.key
     }, {
       primary: selectedField.name
-    }).then(removeDialog).catch(function (error) {
-      requestCreator('instant', JSON.stringify({
-        message: error
-      }))
-    })
+    }).then(removeDialog).catch(handleError)
   }
 }
 
@@ -1546,11 +1513,7 @@ function updateCreateContainer(recordCopy, db) {
   document.getElementById('backToConv').addEventListener('click', function () {
     updateLocalRecord(record, db).then(function () {
       backNav()
-    }).catch(function (error) {
-      requestCreator('instant', JSON.stringify({
-        message: error
-      }))
-    })
+    }).catch(handleError)
   })
 
 
@@ -1674,11 +1637,7 @@ function updateCreateActivity(record) {
               document.getElementById('send-activity').classList.remove('hidden')
             }
           }
-        }).catch(function (error) {
-          requestCreator('instant', JSON.stringify({
-            message: error
-          }))
-        })
+        }).catch(handleError)
       });
     }
 
@@ -2256,9 +2215,7 @@ function createAssigneeList(record, showLabel, db) {
   record.assignees.forEach(function (number) {
     getUserRecord(db, number).then(function (record) {
       parent.appendChild(createSimpleAssigneeLi(record))
-    }).catch(function (error) {
-      requestCreator('instant', JSON.stringify(error))
-    })
+    }).catch(handleError)
   })
 }
 
@@ -2472,10 +2429,7 @@ function readCameraFile() {
     try {
       AndroidInterface.startCamera()
     } catch (e) {
-      requestCreator('instant', JSON.stringify({
-        message: e.message,
-        device: native.getInfo()
-      }));
+      handleError(e.message)
     }
   } else {
     webkit.messageHandlers.takeImageForAttachment.postMessage("convert image to base 64")
@@ -2857,12 +2811,7 @@ function initializeAutocompleteGoogle(autocomplete, record, attr) {
     updateDomFromIDB(record, {
       hash: 'venue',
       key: attr.key
-    }, selectedAreaAttributes).then(removeDialog).catch(function (error) {
-      requestCreator('instant', JSON.stringify({
-        message: error
-      }))
-    })
-
+    }, selectedAreaAttributes).then(removeDialog).catch(handleError)
   })
 }
 
