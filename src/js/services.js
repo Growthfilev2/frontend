03c5efe3f8c68017f20cf5038aef735c14a171e8
shikhar0507 <kap.shikhar@gmail.com>
2019-02-12 17:58:47 +0530

    changed api key in build_prod

--- src/js/services.js
+++ src/js/services.js
@@ -290,7 +290,7 @@ function getCellTowerInfo() {
       return
     }
 
-    var apiKey ='AIzaSyCoGolm0z6XOtI_EYvDmxaRJV_uIVekL_w'
+    var apiKey ='AIzaSyA4s7gp7SFid_by1vLVZDmcKbkEcsStBAo'
     const req = {
       method: 'POST',
       url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + apiKey,
