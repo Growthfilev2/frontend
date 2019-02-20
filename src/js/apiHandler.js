964c1cc9a9d3db2082a3114772c77fa934876107
shikhar0507 <kap.shikhar@gmail.com>
2019-02-20 17:37:23 +0530

    only addendum with isComment prop true is added to list store

--- src/js/apiHandler.js
+++ src/js/apiHandler.js
@@ -1,4 +1,4 @@
-importScripts('external/js/moment.min.js');
+importScripts('../external/js/moment.min.js');
 const apiUrl = 'https://us-central1-growthfilev2-0.cloudfunctions.net/api/'
 
 let deviceInfo;
@@ -717,8 +717,11 @@ function successResponse(read, param) {
           })
         }
       }
-      let key = addendum.activityId
-      counter[key] = (counter[key] || 0) + 1
+
+     if(addendum.isComment) {
+       let key = addendum.activityId 
+       counter[key] = (counter[key] || 0) + 1
+      }
       addendumObjectStore.add(addendum)
     })
 
