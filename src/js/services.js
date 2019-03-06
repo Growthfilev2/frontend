cbcc84b3a7731c4afd40444d517288a1103df4cc
shikhar0507 <kap.shikhar@gmail.com>
2019-03-06 16:21:02 +0530

    Remvoed check for accuracy less than 350 for HTML5 location

--- src/js/services.js
+++ src/js/services.js
@@ -373,7 +373,6 @@ function html5Geolocation() {
         if (stabalzied.length > 1) {
           i++
           if (stabalzied[i].latitude.toFixed(3) === position.coords.latitude.toFixed(3) && stabalzied[i].longitude.toFixed(3) === position.coords.longitude.toFixed(3)) {
-            if (position.coords.accuracy <= 350) {
               stabalizedCount++
               if (stabalizedCount == 3) {
                 clearInterval(interval)
@@ -385,7 +384,6 @@ function html5Geolocation() {
                   provider: 'HMTL5'
                 })
               }
-            }
           }
         }
       }, function (error) {
