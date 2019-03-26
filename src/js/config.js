d89fbab73635e82f5675423905f2a3bfd191a18e
shikhar0507 <kap.shikhar@gmail.com>
2019-03-26 18:54:03 +0530

    Fixed prod keys in config

--- src/js/config.js
+++ src/js/config.js
@@ -1,22 +1,25 @@
-function AppKeys(){
-     this.mode = 'dev'
+function AppKeys() {
+    this.mode = 'dev'
 }
-AppKeys.prototype.getMode = function(){
+AppKeys.prototype.getMode = function () {
     return this.mode
 }
-AppKeys.prototype.getMapKey  = function(){
-    if(this.mode === 'production') {
+AppKeys.prototype.getMapKey = function () {
+    if (this.mode === 'production') {
         return "AIzaSyA4s7gp7SFid_by1vLVZDmcKbkEcsStBAo"
     }
     return "AIzaSyCadBqkHUJwdcgKT11rp_XWkbQLFAy80JQ"
 }
-AppKeys.prototype.getKeys = function(){
-    if(this.mode === 'production') {
+AppKeys.prototype.getKeys = function () {
+    if (this.mode === 'production') {
         return {
             apiKey: this.getMapKey(),
-            authDomain: "",
-            projectId: "growthfile-207204",
-            messagingSenderId: "701025551237"
+            authDomain: 'growthfile-207204.firebaseapp.com',
+            databaseURL: 'https://growthfile-207204.firebaseio.com',
+            projectId: 'growthfile-207204',
+            storageBucket: 'growthfile-207204.appspot.com',
+            messagingSenderId: '701025551237'
+
         }
     }
     return {
@@ -24,9 +27,9 @@ AppKeys.prototype.getKeys = function(){
         authDomain: "growthfilev2-0.firebaseapp.com",
         projectId: "growthfilev2-0",
         messagingSenderId: "1011478688238"
-      }
+    }
 }
-AppKeys.prototype.getBaseUrl = function(){
+AppKeys.prototype.getBaseUrl = function () {
     return this.mode === 'production' ? 'https://api2.growthfile.com/api/' : 'https://us-central1-growthfilev2-0.cloudfunctions.net/api/'
-    
+
 }
