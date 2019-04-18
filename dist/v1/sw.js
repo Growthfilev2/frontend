3366ba6d6b30c05bc5b521b1714508824b15205c
shikhar0507 <kap.shikhar@gmail.com>
2019-04-18 18:21:17 +0530

    service wrker cache name updated

--- dist/v1/sw.js
+++ dist/v1/sw.js
@@ -1,6 +1,6 @@
 // new version of service worker is installed. Hello This is a new file
 
-var CACHE_NAME = 'gf-3'
+var CACHE_NAME = 'gf-1'
 const cacheToAdd = [
     '/v1/',
     '/v1/external/js/firebase-app.js',
