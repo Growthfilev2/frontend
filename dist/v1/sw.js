893999b258dc5a550f73570d17b1ea87f86e6949
shikhar0507 <kap.shikhar@gmail.com>
2019-05-01 18:14:17 +0530

    removed query of gelocation without cell tower and simplified handleGeolocation method

--- dist/v1/sw.js
+++ dist/v1/sw.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 // Service Worker Version 9
 
 var CACHE_NAME = 'gf-9'
-=======
-// Service Worker Version 75
-
-var CACHE_NAME = 'gf-75'
->>>>>>> development
 const cacheToAdd = [
     '/v1/',
     '/v1/external/js/firebase-app.js',
