4ca43ba1754fb53948a77687aac99ec79c5217a2
shikhar0507 <kap.shikhar@gmail.com>
2019-05-03 13:41:18 +0530

    upgrade sw cversion

--- dist/v1/sw.js
+++ dist/v1/sw.js
@@ -1,6 +1,6 @@
-// Service Worker Version 76
+// Service Worker Version 82
 
-var CACHE_NAME = 'gf-76'
+var CACHE_NAME = 'gf-82'
 const cacheToAdd = [
     '/v1/',
     '/v1/external/js/firebase-app.js',
