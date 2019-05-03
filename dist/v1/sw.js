fe22a89473edf200799e78dcde18829a0b0bcb1a
shikhar0507 <kap.shikhar@gmail.com>
2019-05-03 14:07:36 +0530

    branch swithc

--- dist/v1/sw.js
+++ dist/v1/sw.js
@@ -1,6 +1,6 @@
-// Service Worker Version 82
+// Service Worker Version 83
 
-var CACHE_NAME = 'gf-82'
+var CACHE_NAME = 'gf-83'
 const cacheToAdd = [
     '/v1/',
     '/v1/external/js/firebase-app.js',
