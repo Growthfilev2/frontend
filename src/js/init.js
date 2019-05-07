36929089368d7192d928c6f5087f11188eb63d22
shikhar0507 <kap.shikhar@gmail.com>
2019-05-07 18:16:53 +0530

    added view for unsupported features

--- src/js/init.js
+++ src/js/init.js
@@ -141,13 +141,37 @@ window.addEventListener("load", function () {
     ]
   })
 
-  if (!window.Worker && !window.indexedDB) {
-    //TODO: show view instead of dialog
-    return;
-  }
+  if (!window.Worker && !window.indexedDB) return document.getElementById('app-current-panel').appendChild(appIncompatible())
   startApp(true)
 })
 
+function appIncompatible(){
+  document.getElementById('start-loader').classList.add('hidden')
+  const banner = createElement('div',{className:'banner'})
+  const message = createElement('div',{className:'message'});
+  const h2 = createElement('h2',{textContent:'Device Support',className:'mdc-typography--headline3'})
+  message.appendChild(h2)
+  
+  const device = native.getInfo();
+  let bodyMessage;
+  if(!device) {
+    bodyMessage = 'This Device is not Supported'
+  }
+  else {
+    const os = JSON.parse(device).baseOs
+    if(os === 'android') {
+      bodyMessage = 'Growthfile requires Android version 5.1 to run.'
+    }
+    if(os === 'Ios') {
+      bodyMessage = 'Growthfile requires iOS version equal or greater than 12 to run.'
+    }
+  }
+
+  const p = createElement('p',{textContent:bodyMessage})
+  message.appendChild(p)
+  banner.appendChild(message);
+  return banner;
+}
 
 function resetScroll() {
   scroll_namespace.count = 0;
