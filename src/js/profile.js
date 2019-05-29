7a94558dc1425c749369162bdd8dcd484343ad89
shikhar0507 <kap.shikhar@gmail.com>
2019-05-29 11:43:04 +0530

    branch switch

--- src/js/profile.js
+++ src/js/profile.js
@@ -42,28 +42,15 @@ function profileView(pushState) {
               <div class="mdc-tab-bar pb-20" role="tablist">
                   <div class="mdc-tab-scroller">
                       <div class="mdc-tab-scroller__scroll-area">
-                          <div class="mdc-tab-scroller__scroll-content">
-                              ${getUniqueOfficeCount().then(function(offices){
-                                  
-                                  offices.forEach(function(office){
-                                   ` <button class="mdc-tab mdc-tab--active" role="tab" aria-selected="true"
-                                    tabindex="0">
-                                    <span class="mdc-tab__content">
-                                        <span class="mdc-tab__text-label">office</span>
-                                    </span>
-                                    <span class="mdc-tab-indicator mdc-tab-indicator--active">
-                                        <span
-                                            class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
-                                    </span>
-                                    <span class="mdc-tab__ripple"></span>
-                                </button>`
-                                  })
-                              })}
+                          <div class="mdc-tab-scroller__scroll-content" id='tab-scroller'>
+                              
                           </div>
                       </div>
                   </div>
               </div>`
 document.getElementById('app-current-panel').innerHTML = template;
+addOfficesToTabs();
+
 {/* <div class="office-info seperator">
 
 <h1 class="mdc-typography--subtitle1 mt-0">
@@ -181,4 +168,149 @@ This will be displayed on your public profile
 
 </div>
 </div> */}
+}
+
+function addOfficesToTabs(){
+  let template;
+  getUniqueOfficeCount().then(function(offices){
+    template = `${offices.map(function(office,idx){
+      console.log(office)
+       return ` <button class="mdc-tab mdc-tab--active" role="tab" aria-selected="${idx ? 'false':'true'}"
+        tabindex="${idx ? '-1' :'0'}">
+        <span class="mdc-tab__content">
+            <span class="mdc-tab__text-label">${office}</span>
+        </span>
+        <span class="${idx ? 'mdc-tab-indicator' :'mdc-tab-indicator mdc-tab-indicator--active'}">
+            <span
+                class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+        </span>
+        <span class="mdc-tab__ripple"></span>
+    </button>`
+      }).join("")}`
+      
+      document.getElementById('tab-scroller').innerHTML = template;
+      const tabInit = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
+      console.log(tabInit);
+      
+  })
+}
+
+function fillUserDetails(){
+  /* <div class="office-info seperator">
+
+<h1 class="mdc-typography--subtitle1 mt-0">
+    Designation : Sales
+
+</h1>
+<h1 class="mdc-typography--subtitle1 mt-0">
+    Department : Product
+</h1>
+
+<h1 class="mdc-typography--subtitle1 mt-0">
+    Branch office : Co-Workin
+</h1>
+<h1 class="mdc-typography--subtitle1 mt-0">
+    Joined : 5th September, 2018
+</h1>
+<h1 class="mdc-typography--subtitle1"><span>Daily Start Time :</span> 9:30 AM</h1>
+<h1 class="mdc-typography--subtitle1"><span>Daily End Time :</span> 18:30 PM</h1>
+<h1 class="mdc-typography--subtitle1 mt-0">
+    Reports :
+    <span>Footprints</span>
+    <span class="dot"></span>
+    <span>SignUp</span>
+</h1>
+</div>
+<div class="hierchy pt-10">
+<span class="mdc-typography--headline6 mt-0 mb-0">Supervisiors</span>
+<div class="mdc-chip-set supervisor">
+
+    <div class="mdc-chip">
+        <img class="mdc-chip__icon mdc-chip__icon--leading" src="sample.jpeg">
+        <div class="mdc-chip__text">John Doe</div>
+
+    </div>
+    <div class="mdc-chip">
+        <i class="material-icons mdc-chip__icon mdc-chip__icon--leading">supervisor_account</i>
+        <div class="mdc-chip__text">+91900000000</div>
+
+    </div>
+</div>
+
+<span class="mdc-typography--headline6 mt-0 mb-0">Team</h1>
+    <div class="mdc-chip-set supervisor">
+            
+            <div class="mdc-chip">
+                <img class="mdc-chip__icon mdc-chip__icon--leading" src="sample.jpeg">
+                <div class="mdc-chip__text">Syd</div>
+
+            </div>
+            <div class="mdc-chip">
+                <i
+                    class="material-icons mdc-chip__icon mdc-chip__icon--leading">supervisor_account</i>
+                <div class="mdc-chip__text">Gilmour</div>
+
+            </div>
+            <div class="mdc-chip">
+                    <img class="mdc-chip__icon mdc-chip__icon--leading" src="sample.jpeg">
+                <div class="mdc-chip__text">Waters</div>
+
+            </div>
+            <div class="mdc-chip">
+                <i class="material-icons mdc-chip__icon mdc-chip__icon--leading">supervisor_account</i>
+                <div class="mdc-chip__text">+9199999288925</div>
+            </div>
+            <div class="mdc-chip">
+                    <i class="material-icons mdc-chip__icon mdc-chip__icon--leading">add</i>
+                    <div class="mdc-chip__text"> 10 Others</div>
+            </div>
+        </div>
+
+</div>
+<div class="meta-hidden-details" style="border-top: 1px solid rgba(0, 0, 0, 0.2)">
+
+<h1 class="mdc-typography--headline6 mb-0">
+    Remaining Leaves
+    <h1 class="mdc-typography--headline6 mt-0 mb-0">Casual : 5</h1>
+    <h1 class="mdc-typography--headline6 mt-0 mb-0"> Medical : 10</h1>
+</h1>
+</div>
+
+</div>
+
+</div>
+<div class="mdc-typography mdc-typography--body2 p-10 hidden" id='card-body-edit'>
+<div class="mdc-text-field mdc-text-field--with-leading-icon full-width" id='name'>
+
+<i class="material-icons mdc-text-field__icon">account_circle</i>
+<input class="mdc-text-field__input">
+<div class="mdc-line-ripple"></div>
+<label class="mdc-floating-label">Name</label>
+</div>
+<div class="mdc-text-field-helper-line">
+<div id="username-helper-text" class="mdc-text-field-helper-text" aria-hidden="true">
+<i class="material-icons"></i>
+</div>
+</div>
+
+<div class="mdc-text-field mdc-text-field--with-leading-icon full-width" id='email'>
+<i class="material-icons mdc-text-field__icon">email</i>
+<input class="mdc-text-field__input">
+<div class="mdc-line-ripple"></div>
+<label class="mdc-floating-label">Email</label>
+</div>
+<div class="mdc-text-field-helper-line">
+<div id="username-helper-text" class="mdc-text-field-helper-text" aria-hidden="true">
+This will be displayed on your public profile
+</div>
+</div>
+</div>
+</div>
+<div class="mdc-card__actions">
+<div class="mdc-card__action-buttons">
+<span class="mdc-typography--headline6 last-logged-in-time"></span>
+</div>
+
+</div>
+</div> */
 }
