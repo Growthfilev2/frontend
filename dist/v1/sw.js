77b096e4e53706568264c6e5e0434f8d846e4ceb
shikhar0507 <kap.shikhar@gmail.com>
2019-06-09 23:31:15 +0530

    finished

--- dist/v1/sw.js
+++ dist/v1/sw.js
@@ -1,6 +1,6 @@
-// Service Worker Version 28
+// Service Worker Version 30
 
-var CACHE_NAME = 'gf-28'
+var CACHE_NAME = 'gf-30'
 const cacheToAdd = [
     '/v1/',
     '/v1/external/js/firebase-app.js',
