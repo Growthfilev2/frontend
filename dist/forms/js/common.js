216783c0782c4b719339e6e239bb782688242c5d
shikhar0507 <kap.shikhar@gmail.com>
2019-07-15 13:34:35 +0530

    refactored customer

--- dist/forms/js/common.js
+++ dist/forms/js/common.js
@@ -22,3 +22,33 @@ function showSecondDate(event) {
     document.querySelector('.end-date-container').classList.remove('hidden');
     
 }
+
+
+function getDropDownContent(office,template,indexName){
+    return new Promise(function(resolve,reject){
+        const result = {
+            data:[],
+            string:''
+        }
+        const tx = parent.db.transaction(['children'])
+        const keyRange = IDBKeyRange.only([office, template])
+        tx.objectStore('children').index(indexName).openCursor(keyRange).onsuccess = function (event) {
+            const cursor = event.target.result;
+            console.log(cursor)
+            if (!cursor) return;
+            if (cursor.value.status === 'CANCELLED') {
+                cursor.continue();
+                return;
+            }
+          
+            result.string += ` <option value="${cursor.value.attachment.Name.value}">
+                    ${cursor.value.attachment.Name.value}
+                  </option>`
+            cursor.continue();
+        }
+        tx.oncomplete = function () {
+          return resolve(result)
+        }
+     
+    })
+}
