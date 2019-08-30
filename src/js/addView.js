57ec33992069ec897b85e21910cfd2ae72f7491b
shikhar0507 <kap.shikhar@gmail.com>
2019-08-30 19:06:46 +0530

    removed versionNumber for html forms

--- src/js/addView.js
+++ src/js/addView.js
@@ -8,9 +8,9 @@ function addView(sub) {
 
     document.getElementById('app-current-panel').innerHTML = `
     <div class='banner'></div>
-    <iframe id='form-iframe' src='${window.location.origin}/v2/forms/${sub.template}/edit.html?var=${ApplicationState.iframeVersion}'></iframe>
+    <iframe id='form-iframe' src='${window.location.origin}/v2/forms/${sub.template}/edit.html'></iframe>
     `
-    console.log(db)
+
     document.getElementById('form-iframe').addEventListener("load", ev => {
         document.getElementById('form-iframe').contentWindow.init(sub);
     })
