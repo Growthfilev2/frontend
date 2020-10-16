6b9e215dd540fabf728e1d83e4360ec0e4c210e9
Aditya Digant <adityadigant96@yahoo.com>
2020-10-16 17:11:22 +0530

    MDC Top Bar, OTP Issue, Phone number validation and UI fixes

--- src/js/profile_employee.js
+++ src/js/profile_employee.js
@@ -1,65 +1,65 @@
 
-let db;
+// let db;
 
-navigator.serviceWorker.onmessage = (event) => {
-  console.log("message from worker", event.data);
-};
+// navigator.serviceWorker.onmessage = (event) => {
+//   console.log("message from worker", event.data);
+// };
 
-
-window.addEventListener("load", (ev) => {
-  firebase.auth().onAuthStateChanged((user) => {
-    const req = window.indexedDB.open(user.uid);
-    req.onsuccess = function (e) {
-     readdata();
-    };
-    
-  });
-});
-
-
-function readdata(){
-  var transaction = db.transaction(["children"], "readwrite");
-  
-  var objectStore = transaction.objectStore('children');
-
-  var objectStoreRequest = objectStore.get("phoneNumber");
-
-  
-
-  request.onerror = function(event) {
-    console.log('Transaction failed');
-  };
-
-  objectStoreRequest.onsuccess = function(event) {
-    
-    document.getElementById("designation").innerHTML= "hello";
-
-    
-  };
-}
 
 // window.addEventListener("load", (ev) => {
-//   const dbName = firebase.auth().currentUser.uid;
-//   var request = window.indexedDB.open(dbName, 33);
+//   firebase.auth().onAuthStateChanged((user) => {
+//     const req = window.indexedDB.open(user.uid);
+//     req.onsuccess = function (e) {
+//      readdata();
+//     };
+    
+//   });
+// });
 
-//   request.onsuccess = function(event) {
-//     db = event.target.result;
-//   };
 
-//   var transaction = db.transaction('children');
+// function readdata(){
+//   var transaction = db.transaction(["children"], "readwrite");
+  
 //   var objectStore = transaction.objectStore('children');
-//   var request = objectStore.get(phoneNumber);
+
+//   var objectStoreRequest = objectStore.get("phoneNumber");
+
+  
 
 //   request.onerror = function(event) {
 //     console.log('Transaction failed');
 //   };
 
-//   request.onsuccess = function( event) {
-//      if (request.result) {
-//       document.getElementById("designation").innerHTML= request.result.designation.value;
-         
-//      } else {
-//        console.log('No data record');
-//      }
+//   objectStoreRequest.onsuccess = function(event) {
+    
+//     document.getElementById("designation").innerHTML= "hello";
+
+    
 //   };
-// });
+// }
+
+// // window.addEventListener("load", (ev) => {
+// //   const dbName = firebase.auth().currentUser.uid;
+// //   var request = window.indexedDB.open(dbName, 33);
+
+// //   request.onsuccess = function(event) {
+// //     db = event.target.result;
+// //   };
+
+// //   var transaction = db.transaction('children');
+// //   var objectStore = transaction.objectStore('children');
+// //   var request = objectStore.get(phoneNumber);
+
+// //   request.onerror = function(event) {
+// //     console.log('Transaction failed');
+// //   };
+
+// //   request.onsuccess = function( event) {
+// //      if (request.result) {
+// //       document.getElementById("designation").innerHTML= request.result.designation.value;
+         
+// //      } else {
+// //        console.log('No data record');
+// //      }
+// //   };
+// // });
