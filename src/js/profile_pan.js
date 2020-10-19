88301485ae19efc4000479da0623b628cee0b430
shikhar0507 <kap.shikhar@gmail.com>
2020-10-19 21:22:57 +0530

    added image upload

--- src/js/profile_pan.js
+++ src/js/profile_pan.js
@@ -6,12 +6,15 @@ window.addEventListener('load', (ev) => {
       db = req.result;
       getRootRecord().then(loadPanDetail).catch(console.error);
       const submitBtn = document.getElementById('submit-btn');
-      document.getElementById('pan-upload').addEventListener('change',(ev)=>{
-        
+      document.getElementById('pan-upload').addEventListener('change',(evt)=>{
+        getImageBase64(evt).then(function (dataURL) {
+          loadImage(`${dataURL}`)
+        });
       })
       document.getElementById('pan-form').addEventListener('submit',(ev)=>{
         ev.preventDefault();
-        submitBtn.classList.add('in-progress')
+        submitBtn.classList.add('in-progress');
+        
       })
     }
   })
@@ -20,6 +23,11 @@ window.addEventListener('load', (ev) => {
 const loadPanDetail = (rootRecord) => {
   const panTextField = new mdc.textField.MDCTextField(document.getElementById('pan-number'))
   const panData = rootRecord.pan;
-  panTextField.value = panData.number
-  document.querySelector('.upload-image').src = panData.front;
+  panTextField.value = panData.number;
+    loadImage(panData.front)
+}
+
+const loadImage = (src = './img/pan_sample.png') => {
+  document.querySelector('.upload-image').src = src;
+
 }
