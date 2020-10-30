6f55d7c732ccbaea79a3f7bddf13a73743883f3a
shikhar0507 <kap.shikhar@gmail.com>
2020-10-30 14:04:37 +0530

    sw version prod

--- dist/v3/sw.js
+++ dist/v3/sw.js
@@ -36,7 +36,7 @@ const files = [appKey.getMode() === 'dev' ? '/' : '/v3/',
     'external/img/flags@2x.png',
     'external/css/intlTelInput.css',
 ]
-const staticCacheName = 'pages-cache-v655';
+const staticCacheName = 'pages-cache-v656';
 
 // Listen for install event, set callback
 self.addEventListener('install', function (event) {
