ab8a50e804f0f48f3c6b7d61b6b4e34223bead69
Aditya Digant <adityadigant96@yahoo.com>
2020-10-31 19:36:01 +0530

    checkins card UI

--- src/js/jobview.js
+++ src/js/jobview.js
@@ -14,7 +14,7 @@ window.addEventListener("load", (ev) => {
     request.onsuccess = function (event) {
       db = event.target.result;
 
-    //  currenDuty();
+      //  currenDuty();
       checkins_box(jobview_duty);
     };
 
@@ -35,7 +35,7 @@ window.addEventListener("load", (ev) => {
 // function currenDuty() {
 //   getCurrentJob().then((record) => {
 //     if (!record.activityId || record.finished == true) {
-    
+
 //       document.getElementById("current_duty_card").style.display = "none";
 
 //       return;
@@ -88,67 +88,56 @@ window.addEventListener("load", (ev) => {
 // }
 
 function checkins_box(duty) {
-
-if(duty.header == 'CurrentDuty'){
-  document.getElementById("current_duty_heading").innerHTML = 'Current Duty';
-}
-  
+  if (duty.header == "CurrentDuty") {
+    document.getElementById("current_duty_heading").innerHTML = "Current Duty";
+  }
 
   document.getElementById("current_location").innerHTML =
-  duty.attachment.Location.value;
-//console.log(record);
-document.getElementById("starting_time").innerHTML = moment(
-  duty.schedule[0].startTime
-).format("hh:mm A");
+    duty.attachment.Location.value;
+  //console.log(record);
+  document.getElementById("starting_time").innerHTML = moment(
+    duty.schedule[0].startTime
+  ).format("hh:mm A");
 
-document.getElementById("ending_time").innerHTML = moment(
-  duty.schedule[0].endTime
-).format("hh:mm A");
+  document.getElementById("ending_time").innerHTML = moment(
+    duty.schedule[0].endTime
+  ).format("hh:mm A");
 
-if (duty.schedule[0].endTime !== duty.schedule[0].startTime) {
-  document
-    .querySelector(".active-duty--duration")
-    .classList.remove("hidden");
-  document.getElementById("total_time").innerHTML = moment
-    .utc(
-      moment(duty.schedule[0].endTime  - duty.schedule[0].startTime
-      )
-    )
-    .format("HH:mm");
-}
-
-if (duty.assignees[0].displayName) {
-  document.getElementById("assignees_name").innerHTML =
-    duty.assignees[0].displayName;
-} else {
-  document.getElementById("assignees_name").innerHTML =
-    duty.assignees[0].phoneNumber;
-}
-
-if (duty.assignees.length > 1) {
-  if (duty.assignees.length == 2) {
-    document.getElementById("other_assignees").innerHTML =
-      "  &" + duty.assignees.length - 1 + "Other";
-  } else {
-    document.getElementById("other_assignees").innerHTML =
-      "  &" + duty.assignees.length - 1 + "Others";
+  if (duty.schedule[0].endTime !== duty.schedule[0].startTime) {
+    document.querySelector(".active-duty--duration").classList.remove("hidden");
+    document.getElementById("total_time").innerHTML = moment
+      .utc(moment(duty.schedule[0].endTime - duty.schedule[0].startTime))
+      .format("HH:mm");
   }
-}
 
-document.getElementById("assignees_pic").src = duty.assignees[0].photoURL;
+  if (duty.assignees[0].displayName) {
+    document.getElementById("assignees_name").innerHTML =
+      duty.assignees[0].displayName;
+  } else {
+    document.getElementById("assignees_name").innerHTML =
+      duty.assignees[0].phoneNumber;
+  }
 
+  if (duty.assignees.length > 1) {
+    if (duty.assignees.length == 2) {
+      document.getElementById("other_assignees").innerHTML =
+        "  & " + (duty.assignees.length-1) + " Other";
+       
+    } else {
+      document.getElementById("other_assignees").innerHTML =
+        "  & " + (duty.assignees.length-1) + " Others";
+    }
+  }
 
+  document.getElementById("assignees_pic").src = duty.assignees[0].photoURL;
 
   for (i = 0; i < duty.checkins.length; i++) {
-    duty.checkins[i].attachment.Photo.value =
-      "https://www.sammobile.com/wp-content/uploads/2019/03/keyguard_default_wallpaper.png";
+   // duty.checkins[i].attachment.Photo.value ="https://www.sammobile.com/wp-content/uploads/2019/03/keyguard_default_wallpaper.png";
 
     var div = createElement("div", {
       className: "containers",
       style: "display:inline;",
     });
-
-
 
     div.innerHTML = `<div id="container"> ${moment(
       duty.checkins[i].timestamp
@@ -156,23 +145,25 @@ document.getElementById("assignees_pic").src = duty.assignees[0].photoURL;
 
     document.getElementById("time").appendChild(div);
 
+    if (duty.checkins[i].attachment.Photo.value) {
       var photos = createElement("div", {
-        className: "upload_photo"
-       
-      })
+        className: "upload_photo",
+      });
 
-      photos.innerHTML = `<div id="photo_section"><div id="photo"><img id="one_pic" src="${duty.checkins[i].attachment.Photo.value}" alt="" width="160px"
+      photos.innerHTML = `<div id="photo_section"><div id="photo" ><img id="one_pic" src="${
+        duty.checkins[i].attachment.Photo.value
+      }" alt="" width="160px"
       height="94px"></div>
       <span class="material-icons" id="gallery_logo">
       insert_photo
       </span>
-<span id="timestamp_below">${moment(
-  duty.checkins[i].timestamp
-).format("HH:mm A")}</span></div>`
+      <span id="timestamp_below">${moment(duty.checkins[i].timestamp).format(
+        "HH:mm A"
+      )}</span></div>`;
 
       document.getElementById("upload_pics").appendChild(photos);
-
-
+   
+    }
   }
 
   console.log(duty);
