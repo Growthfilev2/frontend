e86c10a119a9a207817d331622ad0ce32171d71f
shikhar0507 <kap.shikhar@gmail.com>
2020-12-23 10:01:01 +0530

    upgraded cache version

--- dist/sw.js
+++ dist/sw.js
@@ -36,7 +36,7 @@ const files = ['/',
     'external/img/flags@2x.png',
     'external/css/intlTelInput.css',
 ]
-const staticCacheName = 'pages-cache-v4005';
+const staticCacheName = 'pages-cache-v4006';
 
 // Listen for install event, set callback
 self.addEventListener('install', function (event) {
